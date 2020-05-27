f23e785be4f2b1353cba04df691fe181917f2b5c
Miguel Chiau <chiau.miguel@gmail.com>
2020-05-27 09:55:54 -0700

    proj components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>The Users Page Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Miguel Chiau",
+      image:
+        "https://www.google.com/search?q=maputo+sunrise&safe=strict&rlz=1C5CHFA_enCA850CA850&sxsrf=ALeKk00tKtJxcVLp93tQ4EyRIPM_zsXfmA:1590517211738&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjnpKirktLpAhVGIDQIHWsaCeUQ_AUoAXoECA0QAw&biw=1440&bih=820#imgrc=zMcTdm3MS9_4oM",
+      places: 3,
+    },
+  ];
+
+  return <UsersList />;
 };
 
 export default Users;
