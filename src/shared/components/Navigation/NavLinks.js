8c68c19b1430fadb3a8bab1f6b0801592cc867e2
Miguel Chiau <chiau.miguel@gmail.com>
2020-05-29 10:50:24 -0700

    side drawer

---
+++ src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./NavLinks.css";
+
+const NavLinks = (props) => {
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/" exact>
+          All Users
+        </NavLink>
+      </li>
+      {/* Both my places and add places should only be rendered when loggedIn */}
+      <li>
+        <NavLink to="/u1/places">My Places</NavLink>
+      </li>
+      <li>
+        <NavLink to="/places/new">Add Place</NavLink>
+      </li>
+      <li>
+        <NavLink to="/auth">Authenticate</NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default NavLinks;
