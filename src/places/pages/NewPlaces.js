6c88b5c732e39de952b9178769b82a16391262ac
Miguel Chiau <chiau.miguel@gmail.com>
2020-05-29 10:04:24 -0700

    nav bar

--- src/places/pages/NewPlaces.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NewPlace = () => {
-  return <h2> New Places Page Works!</h2>;
-};
-
-export default NewPlace;
--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,21 +1,23 @@
-import React from "react";
-import "./UsersList.css";
-import UserItem from "./UserItem";
+import React from 'react';
 
-const UsersList = (props) => {
-  // Check if there are users
+import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
+import './UsersList.css';
+
+const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found...</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
 
-  //If there are users then return an unordered list
   return (
     <ul className="users-list">
-      {props.items.map((user) => (
+      {props.items.map(user => (
         <UserItem
           key={user.id}
           id={user.id}
